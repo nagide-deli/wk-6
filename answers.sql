788e8e249302b138a471fbc5b0061547822f5717
Ivy Nagide <139698308+nagide-deli@users.noreply.github.com>
2025-09-08 13:51:11 +0300

    wk-6

---
+++ answers.sql
@@ -0,0 +1,22 @@
+-- question 1
+SELECT e.firstName, e.lastName, e.email, e.officeCode
+FROM employees e
+INNER JOIN offices o
+    ON e.officeCode = o.officeCode;
+
+
+-- question 2
+SELECT p.productName, p.productVendor, p.productLine
+FROM products p
+LEFT JOIN productlines pl
+    ON p.productLine = pl.productLine;
+
+
+question 3
+SELECT o.orderDate, o.shippedDate, o.status, o.customerNumber
+FROM customers c
+RIGHT JOIN orders o
+    ON c.customerNumber = o.customerNumber
+LIMIT 10;
+
+
